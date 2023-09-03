68fb80c7b354ad87b74e862d78d43c74511e8e72
Hakim <81973750+Irnhakim@users.noreply.github.com>
2023-09-03 17:47:52 +0700

    fix: button dashboard petugas & admin
    setiap tombol bisa langsung ke menu yang sesuai

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,12 +3,22 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class DashboardController extends Controller
 {
     public function index()
     {
+        $level = Auth::guard('petugas')->user()->level;
         $title = 'Halaman Utama';
-        return view('petugas.dashboard.index', compact('title'));
+        if (Auth::guard('petugas')->user()->level == 'petugas') {
+            return view('petugas.dashboard.index', compact('title'));
+        } elseif (Auth::guard('petugas')->user()->level == 'admin') {
+            return view('admin.dashboard.index', compact('title'));
+        } else {
+            return redirect(route('login'))->with('error', 'Silahkan login terlebih dahulu.');
+        }
+
     }
+
 }
